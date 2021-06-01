d1999067ebbb56b1770531ee727f8d0d967f5db5
xhevdet20 <xhevdet1996@gail.com>
2021-06-01 16:34:40 +0200

    fracatals in vanilla js

---
+++ app.js
@@ -0,0 +1,49 @@
+const canvas = document.querySelector('#canvas1');
+
+const ctx = canvas.getContext('2d');
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+const maxLevel = 3;
+const branches = 4;
+const sides = Math.floor((Math.random() * 10) +3);
+const spread = (Math.random() * 48) + 0.51;
+
+ctx.translate(canvas.width / 2, canvas.height / 2 );
+const angle = Math.PI * 2  / spread; // 0.51 - 0.99 (4.8)
+
+function drawLine(level){
+    if (level > maxLevel) return;
+
+    ctx.strokeStyle = 'orangered';
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(150, 0);
+    ctx.stroke();
+
+    for (let i = 1; i < branches + 1; i++){
+        ctx.save();
+        ctx.translate(150 * i / (branches +1), 0);
+        ctx.scale(0.5, 0.5);
+        ctx.save();
+    
+
+        ctx.rotate(angle);
+        drawLine(level +1);
+        ctx.restore();
+        ctx.save();
+
+        ctx.rotate(-angle);
+        drawLine(level +1);
+        ctx.restore();
+
+        ctx.restore();
+    }
+}
+
+for (let i = 0; i < sides; i++){
+    drawLine(0);
+    ctx.rotate(Math.PI * 2 / sides);
+}
